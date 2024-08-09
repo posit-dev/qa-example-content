dbca49876d32b74e1b414eec93284965e0f26de9
Chris Mead <chris.mead@posit.co>
2024-08-09 10:53:31 -0600

    More python plotting samples

--- workspaces/python-widgets/ipyleaflet.py
+++ workspaces/python-widgets/ipyleaflet.py
@@ -1,3 +1,5 @@
+# this only works in the console
+
 from ipyleaflet import Map, Marker
 center = (52.204793, 360.121558)
 
@@ -7,3 +9,18 @@ marker = Marker(location=center, draggable=False)
 m.add(marker);
 
 m
+
+
+#############
+
+
+from ipyleaflet import Map, Marker, display
+center = (52.204793, 360.121558)
+map = Map(center=center, zoom=12)
+
+# Add a draggable marker to the map
+# Dragging the marker updates the marker.location value in Python
+marker = Marker(location=center, draggable=True)
+map.add_control(marker)
+
+display(map)
