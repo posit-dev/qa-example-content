b53a9301d95c2ffbfaf89d90af573722b7a49fb3
Chris Mead <chris.mead@posit.co>
2025-02-18 12:45:53 -0700

    tasks addition

--- workspaces/nyc-flights-data-py/flights-data-frame.py
+++ workspaces/nyc-flights-data-py/flights-data-frame.py
@@ -2,3 +2,4 @@ import pandas as pd
 import os
 data_file_path = os.path.join(os.getcwd(), 'data-files', 'flights', 'flights.parquet')
 df = pd.read_parquet(data_file_path, engine='pyarrow')
+print(len(df))
