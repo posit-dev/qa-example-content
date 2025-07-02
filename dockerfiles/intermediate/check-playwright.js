b1ca8f3dd363f600e4cbc123d4a458d9e1cc078d
Chris Mead <chris.mead@posit.co>
2025-07-02 07:32:33 -0600

    Playwright docker sample

---
+++ dockerfiles/intermediate/check-playwright.js
@@ -0,0 +1,9 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
+})();
