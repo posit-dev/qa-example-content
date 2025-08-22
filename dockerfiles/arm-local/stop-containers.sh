893e06cfcb2d5a9ff6dbd8d8321def5fcce5b2b7
Pete <1178561+petetronic@users.noreply.github.com>
2025-08-22 09:20:15 -0400

    Docker cli removed docker-compose in favor of compose option to docker tool (#75)

--- dockerfiles/arm-local/stop-containers.sh
+++ dockerfiles/arm-local/stop-containers.sh
@@ -3,8 +3,8 @@
 # stop-containers.sh - Script to stop and remove Docker containers
 
 echo "Stopping and removing containers..."
-docker-compose down
+docker compose down
 echo "Containers removed."
 
 # Optional: If you want to also remove volumes (uncomment the line below)
-# docker-compose down -v
+# docker compose down -v
