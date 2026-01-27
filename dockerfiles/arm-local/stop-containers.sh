bfe9f7f4917dd9abf3fa5d8fef584b07d20ac784
Christopher Mead <chrisrmead@comcast.net>
2026-01-27 15:34:16 -0700

    Add SLES to arm-local options (#103)

--- dockerfiles/arm-local/stop-containers.sh
+++ dockerfiles/arm-local/stop-containers.sh
@@ -6,10 +6,10 @@
 OS_TYPE="ubuntu24"
 
 # Check for OS type argument
-if [ "$1" = "ubuntu24" ] || [ "$1" = "rocky8" ] || [ "$1" = "opensuse156" ]; then
+if [ "$1" = "ubuntu24" ] || [ "$1" = "rocky8" ] || [ "$1" = "opensuse156" ] || [ "$1" = "sles156" ]; then
   OS_TYPE="$1"
 else
-  echo "Usage: $0 [ubuntu24|rocky8|opensuse156]"
+  echo "Usage: $0 [ubuntu24|rocky8|opensuse156|sles156]"
   echo "       Default is ubuntu24 if not specified"
   echo ""
 fi
